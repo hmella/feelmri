6867d6f8677737f41e976cac523fb15747c1f263
Hernan Mella <hernan.mella@pucv.cl>
2025-05-26 19:41:42 -0400

    Update cpp code and added new functionalities for mesh refinement

--- FEelMRI/src/Tagging.cpp
+++ FEelMRI/src/Tagging.cpp
@@ -1,17 +1,17 @@
 #include <Signal.h>
 #include <Tagging.h>
 
-
-Tensor<std::complex<float>, 4> SPAMM(
+template <typename T>
+Tensor<std::complex<T>, 4> SPAMM(
   const int &MPI_rank,
-  const SparseMatrix<float> &M,
-  const std::vector<Tensor<float, 3>> &kloc,
-  const MatrixXf &t,
-  const MatrixXf &r,
-  const MatrixXcf &M_spamm,
-  const VectorXf &phi_dB0,
-  const float &T2,
-  const VectorXcf &profile
+  const SparseMatrix<T> &M,
+  const std::vector<Tensor<T, 3>> &kloc,
+  const Tensor<T, 3> &t,
+  const Matrix<T, Dynamic, Dynamic> &r,
+  const Matrix<std::complex<T>, Dynamic, Dynamic> &M_spamm,
+  const Vector<T, Dynamic> &phi_dB0,
+  const Vector<T, Dynamic> &T2,
+  const Vector<std::complex<T>, Dynamic> &profile
   ){
 
     // Nb of measurements in the readout direction
@@ -24,30 +24,31 @@ Tensor<std::complex<float>, 4> SPAMM(
     const uint nb_kz = kloc[0].dimension(2);
     
     // Nb of spins    
-    const uint nb_spins = r.rows();
+    const uint nb_nodes = r.rows();
 
     // Nb of encoded velocities
     const uint nb_enc = M_spamm.cols();
 
     // Complex unit
-    const std::complex<float> i1(0.0, 1.0);
+    const std::complex<T> i1(0.0, 1.0);
 
     // Get the equivalent gradient needed to go from the center of the kspace
     // to each location
-    const Tensor<float, 3> kx = 2.0 * PI * kloc[0];
-    const Tensor<float, 3> ky = 2.0 * PI * kloc[1];
-    const Tensor<float, 3> kz = 2.0 * PI * kloc[2];
+    const Tensor<T, 3> kx = 2.0 * PI * kloc[0];
+    const Tensor<T, 3> ky = 2.0 * PI * kloc[1];
+    const Tensor<T, 3> kz = 2.0 * PI * kloc[2];
 
     // Kspace, Fourier exponential, and off-resonance phase
-    MatrixXcf Mxy = 1.0e+3 * nb_spins * M_spamm.array() * profile.array().replicate(1, nb_enc);
-    VectorXcf fourier(nb_spins);
-    VectorXf  phi_off(nb_spins);
+    Matrix<std::complex<T>, Dynamic, Dynamic> Mxy;
+    Mxy = M_spamm.array(); // * profile.array().replicate(1, nb_enc);
+    Vector<std::complex<T>, Dynamic> fourier(nb_nodes);
+    Vector<T, Dynamic>  phi_off(nb_nodes);
 
     // kspace
-    Tensor<std::complex<float>, 4> kspace(nb_meas, nb_lines, nb_kz, nb_enc);
+    Tensor<std::complex<T>, 4> kspace(nb_meas, nb_lines, nb_kz, nb_enc);
 
     // T2* decay
-    const MatrixXf T2_decay = (-t / T2).array().exp();
+    Vector<T, Dynamic> T2_decay(nb_nodes);
 
     // Iterate over kspace phase lines
     uint i, j, k, l;
@@ -59,18 +60,21 @@ Tensor<std::complex<float>, 4> SPAMM(
       // Iterate over kspace readout points
       for (i = 0; i < nb_meas; i++){
 
-        // Update off-resonance phase
-        phi_off.noalias() = phi_dB0 * t(i,j);
-
         // Iterate over slices
         for (k = 0; k < nb_kz; k++){
+
+          // Update off-resonance phase
+          phi_off.noalias() = phi_dB0 * t(i,j,k);
 
           // Calculate Fourier exponential
           fourier = FourierEncoding(r, kx, ky, kz, phi_off, i, j, k);
 
+          // Calculate T2/T2* decay
+          T2_decay = (-t(i,j,k) / T2.array()).exp();
+
           // Calculate k-space values, add T2* decay, and assign value to output array
           for (l = 0; l < nb_enc; l++){
-            kspace(i,j,k,l) = signal(M, Mxy, fourier, T2_decay(i,j), l);
+            kspace(i,j,k,l) = signal(M, Mxy, fourier, T2_decay, l);
           }
         }
       }
--- FEelMRI/src/Tagging.h
+++ FEelMRI/src/Tagging.h
@@ -5,19 +5,41 @@
 #include <pybind11/stl.h>
 #include <Eigen/Dense>
 
-Tensor<std::complex<float>, 4> SPAMM(
+template <typename T>
+Tensor<std::complex<T>, 4> SPAMM(
   const int &MPI_rank,
-  const SparseMatrix<float> &M,
-  const std::vector<Tensor<float, 3>> &kloc,
-  const MatrixXf &t,
-  const MatrixXf &r,
-  const MatrixXcf &M_spamm,
-  const VectorXf &phi_dB0,
-  const float &T2,
-  const VectorXcf &profile
+  const SparseMatrix<T> &M,
+  const std::vector<Tensor<T, 3>> &kloc,
+  const Tensor<T, 3> &t,
+  const Matrix<T, Dynamic, Dynamic> &r,
+  const Matrix<std::complex<T>, Dynamic, Dynamic> &M_spamm,
+  const Vector<T, Dynamic> &phi_dB0,
+  const Vector<T, Dynamic> &T2,
+  const Vector<std::complex<T>, Dynamic> &profile
   );
 
 PYBIND11_MODULE(Tagging, m) {
     m.doc() = "Utilities for MR image generation";
-    m.def("SPAMM", &SPAMM);
+    m.def("SPAMM", py::overload_cast<
+      const int &,
+      const SparseMatrix<double> &,
+      const std::vector<Tensor<double, 3>> &,
+      const Tensor<double, 3> &,
+      const Matrix<double, Dynamic, Dynamic> &,
+      const Matrix<std::complex<double>, Dynamic, Dynamic> &,
+      const Vector<double, Dynamic> &,
+      const Vector<double, Dynamic> &,
+      const Vector<std::complex<double>, Dynamic> &
+    >(&SPAMM<double>));
+    m.def("SPAMM", py::overload_cast<
+      const int &,
+      const SparseMatrix<float> &,
+      const std::vector<Tensor<float, 3>> &,
+      const Tensor<float, 3> &,
+      const Matrix<float, Dynamic, Dynamic> &,
+      const Matrix<std::complex<float>, Dynamic, Dynamic> &,
+      const Vector<float, Dynamic> &,
+      const Vector<float, Dynamic> &,
+      const Vector<std::complex<float>, Dynamic> &
+    >(&SPAMM<float>));
 }
