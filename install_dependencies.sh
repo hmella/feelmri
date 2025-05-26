15047c894edd336c126fb43fea26e72f3f2f082c
Hernan Mella <hernan.mella@pucv.cl>
2025-05-26 19:37:33 -0400

    Added pint to dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@
 sudo apt-get update -y
 sudo apt-get -y install build-essential python3-dev python3-pip python3-tk \
                  python3-setuptools libopenmpi-dev mpich cmake
-sudo pip3 install pybind11 mpi4py meshio scipy scikit-image matplotlib
+sudo pip3 install pybind11 pyyaml mpi4py h5py meshio scipy matplotlib scikit-image pint
 
 # Clone, build and install Eigen3-dev
 git clone https://gitlab.com/libeigen/eigen.git && \
