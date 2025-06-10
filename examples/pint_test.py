02cd75da150f6cf29ac68082480b982e72b77644
Hernan Mella <hernan.mella@pucv.cl>
2025-06-10 01:06:20 -0400

    Removed pint units example

--- examples/pint_test.py
+++
@@ -1,7 +0,0 @@
-from pint import UnitRegistry
-
-# Register units
-ureg = UnitRegistry()
-ureg.define('Tesla = 1 * tesla = T')
-ureg.define('milliTesla = 1e-3 * tesla = mT')
-ureg.define('millisecond = 1e-3 * second = ms')
