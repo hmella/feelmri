3494c725e4ad5d465187ac3bc62ac8865b9e23c3
Hernan Mella <hernan.mella@pucv.cl>
2025-06-22 22:52:39 -0400

    Remove non-used functions

--- feelmri/src/PhaseContrast.h
+++ feelmri/src/PhaseContrast.h
@@ -5,41 +5,6 @@
 #include <pybind11/stl.h>
 #include <Eigen/Dense>
 
-/**
- * @brief Computes the 3D flow image from k-space data.
- *
- * This function calculates the 3D flow image by simulating the k-space 
- * acquisition process. It takes into account the mass matrix, k-space 
- * trajectory, k-space timings, object initial position and velocity, 
- * encoded velocities, field inhomogeneity, T2 time of the blood, velocity 
- * encoding, and slice profile.
- *
- * @param MPI_rank The rank of the MPI process.
- * @param M The mass matrix.
- * @param kloc The k-space trajectory.
- * @param t The k-space timings.
- * @param r0 The initial position of the object.
- * @param v The initial velocity of the object.
- * @param phi_v The encoded velocities.
- * @param phi_dB0 The field inhomogeneity.
- * @param T2 The T2 time of the blood.
- * @param profile The slice profile.
- * @return A 4D tensor representing the k-space data.
- */
-template <typename T>
-Tensor<std::complex<T>, 4> PC(
-  const int &MPI_rank,
-  const SparseMatrix<T> &M,
-  const std::vector<Tensor<T, 3>> &kloc,
-  const Tensor<T, 3> &t,
-  const Matrix<T, Dynamic, Dynamic> &r0,
-  const Matrix<T, Dynamic, Dynamic> &v,
-  const Matrix<T, Dynamic, Dynamic> &phi_v,
-  const Vector<T, Dynamic> &phi_dB0,
-  const Vector<T, Dynamic> &T2,
-  const Vector<std::complex<T>, Dynamic> &profile
-  );
-
 
 /**
  * @brief Computes the 3D flow image from k-space data.
@@ -195,27 +160,6 @@ Tensor<std::complex<T>, 4> PCFat2(
 
 PYBIND11_MODULE(PhaseContrast, m) {
     m.doc() = "Utilities for MR image generation";
-    // Overloaded function for PC with different parameters      
-    m.def("PC", py::overload_cast<const int &,
-      const SparseMatrix<double> &,
-      const std::vector<Tensor<double, 3>> &,
-      const Tensor<double, 3> &,
-      const Matrix<double, Dynamic, Dynamic> &,
-      const Matrix<double, Dynamic, Dynamic> &,
-      const Matrix<double, Dynamic, Dynamic> &,
-      const Vector<double, Dynamic> &,
-      const Vector<double, Dynamic> &,
-      const Vector<std::complex<double>, Dynamic> &>(&PC<double>));
-    m.def("PC", py::overload_cast<const int &,
-      const SparseMatrix<float> &,
-      const std::vector<Tensor<float, 3>> &,
-      const Tensor<float, 3> &,
-      const Matrix<float, Dynamic, Dynamic> &,
-      const Matrix<float, Dynamic, Dynamic> &,
-      const Matrix<float, Dynamic, Dynamic> &,
-      const Vector<float, Dynamic> &,
-      const Vector<float, Dynamic> &,
-      const Vector<std::complex<float>, Dynamic> &>(&PC<float>));
     // Overloaded function for PC with different parameters      
     m.def("PC", py::overload_cast<const int &,
       const SparseMatrix<double> &,
--- feelmri/src/Tagging.h
+++ feelmri/src/Tagging.h
@@ -5,16 +5,18 @@
 #include <pybind11/stl.h>
 #include <Eigen/Dense>
 
+
 template <typename T>
 Tensor<std::complex<T>, 4> SPAMM(
   const int &MPI_rank,
   const SparseMatrix<T> &M,
   const std::vector<Tensor<T, 3>> &kloc,
   const Tensor<T, 3> &t,
-  const Matrix<T, Dynamic, Dynamic> &r0,
-  const Matrix<std::complex<T>, Dynamic, Dynamic> &M_spamm,
+  const Matrix<T, Dynamic, Dynamic> &r,
+  const Matrix<std::complex<T>, Dynamic, Dynamic> &Mxy,
   const Vector<T, Dynamic> &phi_dB0,
-  const Vector<T, Dynamic> &T2
+  const Vector<T, Dynamic> &T2,
+  const py::function &pod_trajectory
   );
 
 template <typename T>
@@ -24,35 +26,13 @@ Tensor<std::complex<T>, 4> SPAMM(
   const std::vector<Tensor<T, 3>> &kloc,
   const Tensor<T, 3> &t,
   const Matrix<T, Dynamic, Dynamic> &r,
-  const Matrix<std::complex<T>, Dynamic, Dynamic> &M_spamm,
+  const Matrix<std::complex<T>, Dynamic, Dynamic> &Mxy,
   const Vector<T, Dynamic> &phi_dB0,
-  const Vector<T, Dynamic> &T2,
-  const py::function &pod_trajectory
+  const Vector<T, Dynamic> &T2
   );
 
 PYBIND11_MODULE(Tagging, m) {
     m.doc() = "Utilities for MR image generation";
-    // Functions without pod_trajectory
-    m.def("SPAMM", py::overload_cast<
-      const int &,
-      const SparseMatrix<double> &,
-      const std::vector<Tensor<double, 3>> &,
-      const Tensor<double, 3> &,
-      const Matrix<double, Dynamic, Dynamic> &,
-      const Matrix<std::complex<double>, Dynamic, Dynamic> &,
-      const Vector<double, Dynamic> &,
-      const Vector<double, Dynamic> &
-    >(&SPAMM<double>));
-    m.def("SPAMM", py::overload_cast<
-      const int &,
-      const SparseMatrix<float> &,
-      const std::vector<Tensor<float, 3>> &,
-      const Tensor<float, 3> &,
-      const Matrix<float, Dynamic, Dynamic> &,
-      const Matrix<std::complex<float>, Dynamic, Dynamic> &,
-      const Vector<float, Dynamic> &,
-      const Vector<float, Dynamic> &
-    >(&SPAMM<float>));
     // Functions with pod_trajectory
     m.def("SPAMM", py::overload_cast<
       const int &,
@@ -75,5 +55,26 @@ PYBIND11_MODULE(Tagging, m) {
       const Vector<float, Dynamic> &,
       const Vector<float, Dynamic> &,
       const py::function &
+    >(&SPAMM<float>));
+    // Functions without pod_trajectory
+    m.def("SPAMM", py::overload_cast<
+      const int &,
+      const SparseMatrix<double> &,
+      const std::vector<Tensor<double, 3>> &,
+      const Tensor<double, 3> &,
+      const Matrix<double, Dynamic, Dynamic> &,
+      const Matrix<std::complex<double>, Dynamic, Dynamic> &,
+      const Vector<double, Dynamic> &,
+      const Vector<double, Dynamic> &
+    >(&SPAMM<double>));
+    m.def("SPAMM", py::overload_cast<
+      const int &,
+      const SparseMatrix<float> &,
+      const std::vector<Tensor<float, 3>> &,
+      const Tensor<float, 3> &,
+      const Matrix<float, Dynamic, Dynamic> &,
+      const Matrix<std::complex<float>, Dynamic, Dynamic> &,
+      const Vector<float, Dynamic> &,
+      const Vector<float, Dynamic> &
     >(&SPAMM<float>));
 }
