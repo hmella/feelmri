85a2aeeab3dced10d6d030d5926d7c45206566a5
Hernan Mella <hernan.mella@pucv.cl>
2025-11-02 20:34:45 -0300

    Update README and install_dependencies.sh script. Remove GPU Dockerfile for first release

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
 # Install feelmri dependencies
-sudo apt-get update -y
-sudo apt-get -y install build-essential python3-dev python3-pip python3-tk \
-                 python3-setuptools libopenmpi-dev mpich cmake
-sudo pip3 install pybind11 pyyaml mpi4py h5py meshio scipy matplotlib scikit-image pint fenics-basix pymetis
-
-# basix installation
-git clone git@github.com:FEniCS/basix.git
-# cd basix/cpp
-# mkdir build/
-# cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -B build -S .
-# cmake --build build
-# sudo cmake --install build
-pip3 install . --user
+sudo apt-get -y install build-essential python3-dev python3-pip python3-tk python3-setuptools libopenmpi-dev cmake
 
 # Eigen3 installation
 git clone https://gitlab.com/libeigen/eigen.git && \
