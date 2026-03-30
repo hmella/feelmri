7e66b637ed5f9b15be2f0aca6867c1eae63c03af
Hernan Mella <hernan.mella@pucv.cl>
2026-03-29 22:37:06 -0300

    Change installation of fenics-basix using pip with compiled C++ version directly from repo

--- python/feelmri/__init__.py
+++ python/feelmri/__init__.py
@@ -4,10 +4,3 @@ try:
     __version__ = version("feelmri")
 except PackageNotFoundError:
     __version__ = "0.0.0"
-
-# Ensure basix (and its bundled libbasix) is loaded before feelmri extensions
-try:
-    import basix  # noqa: F401
-except Exception:
-    # If basix isn't installed, leave the error to the caller when they import features needing it
-    pass
