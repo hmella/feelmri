34048979b1193a6e314c788c501f6aec7b659a85
Hernan Mella <hernan.mella@pucv.cl>
2026-04-27 22:16:25 -0400

    Improve export of images and code reorganization

--- python/feelmri/Plotter.py
+++ python/feelmri/Plotter.py
@@ -1,251 +1,167 @@
 import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-from feelmri.MPIUtilities import MPI_rank
-
-
 class MRIPlotter:
-  """
-  A class to plot MRI images with interactive slice and frame navigation.
+    def __init__(self, images=[], FOV=[1, 1, 1], caxis=None, cmap=plt.get_cmap('Greys_r'), 
+                 title=[], swap_axes=None, shape=None, next_frame_key='d', 
+                 previous_frame_key='a', next_slice_key='w', previous_slice_key='s'):
+        
+        self.images = images
+        self.FOV = np.array(FOV)
+        self.caxis = caxis
+        self.cmap = cmap
+        self.title = title if title else ['',] * len(self.images)
+        self.swap_axes = swap_axes
+        self.shape = shape
+        self.next_frame_key = next_frame_key
+        self.previous_frame_key = previous_frame_key
+        self.next_slice_key = next_slice_key
+        self.previous_slice_key = previous_slice_key
 
-  Attributes:
-    images (list): List of MRI images.
-    FOV (np.array): Field of view dimensions.
-    caxis (list): Color axis limits.
-    cmap (Colormap): Colormap for the images.
-    title (list): Titles for each subplot.
-    swap_axes (tuple): Axes to swap.
-    shape (tuple): Shape of the subplot grid.
-    next_frame_key (str): Key to go to the next frame.
-    previous_frame_key (str): Key to go to the previous frame.
-    next_slice_key (str): Key to go to the next slice.
-    previous_slice_key (str): Key to go to the previous slice.
-  """
+        # Use transpose/swapaxes to create views rather than full copies where possible
+        self._images = [im.swapaxes(0, 1) for im in self.images]
+        self._FOV = self.FOV[[1, 0, 2]]
+        
+        if self.swap_axes is not None:
+            self._images = [im.swapaxes(self.swap_axes[0], self.swap_axes[1]) for im in self._images]
+            self._FOV[self.swap_axes[::-1]] = self._FOV[self.swap_axes]
 
-  def __init__(self, images: list = [], 
-               FOV: list | np.ndarray = [1, 1, 1], 
-               caxis: list | np.ndarray = None, 
-               cmap=plt.get_cmap('Greys_r'), 
-               title: list = [], 
-               swap_axes: list = None, 
-               shape: list | np.ndarray = None, 
-               next_frame_key: str = 'd', 
-               previous_frame_key: str = 'a', 
-               next_slice_key: str = 'w', 
-               previous_slice_key: str = 's'):
-    """
-    Initializes the MRIPlotter with given parameters.
+        self._ensure_4d()
+        self.fig = None
+        self.ax = None
 
-    Args:
-      images (list): List of MRI images.
-      FOV (list): Field of view dimensions.
-      caxis (list): Color axis limits.
-      cmap (Colormap): Colormap for the images.
-      title (list): Titles for each subplot.
-      swap_axes (tuple): Axes to swap.
-      shape (tuple): Shape of the subplot grid.
-      next_frame_key (str): Key to go to the next frame.
-      previous_frame_key (str): Key to go to the previous frame.
-      next_slice_key (str): Key to go to the next slice.
-      previous_slice_key (str): Key to go to the previous slice.
-    """
-    self.images = images
-    self.FOV = np.array(FOV)
-    self.caxis = caxis
-    self.cmap = cmap
-    self.title = title if title else ['',] * len(self.images)
-    self.swap_axes = swap_axes
-    self.shape = shape
-    self.next_frame_key = next_frame_key
-    self.previous_frame_key = previous_frame_key
-    self.next_slice_key = next_slice_key
-    self.previous_slice_key = previous_slice_key
+    def _ensure_4d(self):
+        for i in range(len(self._images)):
+            while self._images[i].ndim < 4:
+                self._images[i] = self._images[i][..., np.newaxis]
 
-    self._images = [im.swapaxes(0, 1) for im in self.images]
-    self._FOV = self.FOV[[1, 0, 2]]
-    if self.swap_axes is not None:
-      self._images = [im.swapaxes(self.swap_axes[0], self.swap_axes[1]) for im in self._images]
-      self._FOV[self.swap_axes[::-1]] = self._FOV[self.swap_axes]
+    def _setup_axes(self):
+        if self.fig is not None:
+            return
+            
+        n = len(self._images)
+        if self.shape is not None:
+            self.fig, self.ax = plt.subplots(self.shape[0], self.shape[1])
+        else:
+            if n < 4:
+                self.fig, self.ax = plt.subplots(1, n)
+            else:
+                cols = int(np.ceil(np.sqrt(n)))
+                rows = int(np.ceil(n / cols))
+                self.fig, self.ax = plt.subplots(rows, cols)
+        
+        if not isinstance(self.ax, np.ndarray):
+            self.ax = np.array([self.ax])
+        self.ax = self.ax.flatten()
 
-    self.check_dimensions()
-    self.create_figures()
+    def export_images(self, output_dir, prefix='im', format='png', dpi=150):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-  def check_dimensions(self):
-    """
-    Ensures all images have 4 dimensions.
-    """
-    while len(self._images[0].shape) < 4:
-      self._images = [im[..., np.newaxis] for im in self._images]
-
-  def create_figures(self):
-    """
-    Creates the figure and axes for plotting.
-    """
-    if self.shape is not None:
-      self.fig, self.ax = plt.subplots(self.shape[0], self.shape[1])
-    else:
-      if len(self.images) < 4:
-        self.fig, self.ax = plt.subplots(1, len(self.images))
-        if len(self.images) == 1:
-          self.ax = np.array([self.ax])
-      else:
-        cols = np.ceil(np.sqrt(len(self.images))).astype(int)
-        rows = np.ceil(len(self.images) / cols).astype(int)
-        self.fig, self.ax = plt.subplots(rows, cols)
-
-  def export_images(self, output_dir, prefix='im', format='png', dpi=150):
-    """
-    Exports all frames and slices of the MRI images to PNG files.
-
-    Args:
-      output_dir (str): Directory to save the exported images.
-    """
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    # Set the extent for the images based on the field of view
-    extent = [0, self._FOV[1], 0, self._FOV[0]]
-
-    for i, im in enumerate(self._images):
-        num_slices = im.shape[2]
-        num_frames = im.shape[3]
-
-        for slice_idx in range(num_slices):
-            for frame_idx in range(num_frames):
-                vmin, vmax = (im.min(), im.max()) if self.caxis is None else (self.caxis[i][0], self.caxis[i][1]) if isinstance(self.caxis[0], list) else (self.caxis[0], self.caxis[1])
-
-                fig, ax = plt.subplots()
-                image = ax.imshow(im[..., slice_idx, frame_idx], cmap=self.cmap, vmin=vmin, vmax=vmax, extent=extent)
-                ax.invert_yaxis()
-                ax.set_title(f'{self.title[i]} - Slice {slice_idx}, Frame {frame_idx}')
-                ax.set_xticklabels([])
-                ax.set_yticklabels([])
-
-                divider = make_axes_locatable(ax)
-                colorbar_axes = divider.append_axes("right", size="10%", pad=0.1)
-                cbar = fig.colorbar(image, cax=colorbar_axes)
-                cbar.minorticks_on()
-
-                filename = os.path.join(output_dir, f'{prefix}_{i}_slice_{slice_idx}_frame_{frame_idx}.{format}')
-                plt.savefig(filename, bbox_inches='tight', dpi=dpi)
-                plt.close(fig)
-
-  def show(self):
-    """
-    Displays the MRI images with interactive navigation.
-    """
-    extent = [0, self._FOV[1], 0, self._FOV[0]]
-    self.remove_keymap_conflicts({self.next_slice_key, self.previous_slice_key, self.next_frame_key, self.previous_frame_key})
-
-    flat_ax = self.ax.flatten()
-    for i, (im, ax) in enumerate(zip(self._images, flat_ax)):
-        ax.im = im
-        ax.slice = 0
-        ax.frame = 0
-
-        vmin, vmax = (im.min(), im.max()) if self.caxis is None else (self.caxis[i][0], self.caxis[i][1]) if isinstance(self.caxis[0], list) else (self.caxis[0], self.caxis[1])
-        image = ax.imshow(im[..., ax.slice, ax.frame], cmap=self.cmap, vmin=vmin, vmax=vmax, extent=extent)
-
-        if len(self._images) < len(flat_ax):
-            [flat_ax[j].axis('off') for j in range(len(self._images), len(flat_ax))]
-
-        ax.invert_yaxis()
-        ax.set_title(self.title[i])
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-
+        extent = [0, self._FOV[1], 0, self._FOV[0]]
+        
+        # Create a persistent figure for exporting to avoid memory fragmentation
+        fig, ax = plt.subplots()
         divider = make_axes_locatable(ax)
-        colorbar_axes = divider.append_axes("right", size="10%", pad=0.1)
-        cbar = self.fig.colorbar(image, cax=colorbar_axes)
-        cbar.minorticks_on()
+        cax = divider.append_axes("right", size="10%", pad=0.1)
+        
+        for i, im in enumerate(self._images):
+            # Resolve vmin/vmax logic once per image set
+            if self.caxis is None:
+                vmin, vmax = im.min(), im.max()
+            elif isinstance(self.caxis[0], (list, np.ndarray)):
+                vmin, vmax = self.caxis[i][0], self.caxis[i][1]
+            else:
+                vmin, vmax = self.caxis[0], self.caxis[1]
 
-    self.fig.canvas.mpl_connect('key_press_event', self.process_key)
+            # Initialize image object once per volume
+            img_obj = ax.imshow(im[..., 0, 0], cmap=self.cmap, vmin=vmin, vmax=vmax, extent=extent)
+            cbar = fig.colorbar(img_obj, cax=cax)
+            cbar.minorticks_on()
+            ax.invert_yaxis()
+            ax.set_xticks([])
+            ax.set_yticks([])
 
-    self.fig.suptitle('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
-    self.fig.tight_layout()
-    plt.show()
+            for s_idx in range(im.shape[2]):
+                for f_idx in range(im.shape[3]):
+                    # Rapid update of data instead of rebuilding plot
+                    img_obj.set_data(im[..., s_idx, f_idx])
+                    ax.set_title(f'{self.title[i]} - Slice {s_idx}, Frame {f_idx}')
+                    
+                    fname = os.path.join(output_dir, f'{prefix}_{i}_s{s_idx}_f{f_idx}.{format}')
+                    fig.savefig(fname, bbox_inches='tight', dpi=dpi)
+            
+            cax.cla() # Clear colorbar axis for the next image set
+            ax.cla()
 
-  def previous_slice(self, ax):
-    """
-    Goes to the previous slice.
+        plt.close(fig)
 
-    Args:
-      ax (Axes): The axis to update.
-    """
-    im = ax.im
-    ax.slice = (ax.slice - 1) % im.shape[2]
-    ax.images[0].set_array(im[..., ax.slice, ax.frame])
-    self.fig.suptitle('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
+    def show(self):
+        self._setup_axes()
+        extent = [0, self._FOV[1], 0, self._FOV[0]]
+        self.remove_keymap_conflicts({self.next_slice_key, self.previous_slice_key, self.next_frame_key, self.previous_frame_key})
 
-  def next_slice(self, ax):
-    """
-    Goes to the next slice.
+        for i, (im, ax) in enumerate(zip(self._images, self.ax)):
+            ax.im_data = im
+            ax.curr_slice = 0
+            ax.curr_frame = 0
 
-    Args:
-      ax (Axes): The axis to update.
-    """
-    im = ax.im
-    ax.slice = (ax.slice + 1) % im.shape[2]
-    ax.images[0].set_array(im[..., ax.slice, ax.frame])
-    self.fig.suptitle('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
+            if self.caxis is None:
+                vmin, vmax = im.min(), im.max()
+            elif isinstance(self.caxis[0], (list, np.ndarray)):
+                vmin, vmax = self.caxis[i][0], self.caxis[i][1]
+            else:
+                vmin, vmax = self.caxis[0], self.caxis[1]
 
-  def previous_frame(self, ax):
-    """
-    Goes to the previous frame.
+            img_obj = ax.imshow(im[..., 0, 0], cmap=self.cmap, vmin=vmin, vmax=vmax, extent=extent)
+            ax.invert_yaxis()
+            ax.set_title(self.title[i])
+            ax.set_xticks([])
+            ax.set_yticks([])
 
-    Args:
-      ax (Axes): The axis to update.
-    """
-    im = ax.im
-    ax.frame = (ax.frame - 1) % im.shape[3]
-    ax.images[0].set_array(im[..., ax.slice, ax.frame])
-    self.fig.suptitle('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
+            divider = make_axes_locatable(ax)
+            cbar_ax = divider.append_axes("right", size="10%", pad=0.1)
+            self.fig.colorbar(img_obj, cax=cbar_ax).minorticks_on()
 
-  def next_frame(self, ax):
-    """
-    Goes to the next frame.
+        # Disable unused axes
+        for j in range(len(self._images), len(self.ax)):
+            self.ax[j].axis('off')
 
-    Args:
-      ax (Axes): The axis to update.
-    """
-    im = ax.im
-    ax.frame = (ax.frame + 1) % im.shape[3]
-    ax.images[0].set_array(im[..., ax.slice, ax.frame])
-    self.fig.suptitle('Slice {:d}, frame {:d}'.format(ax.slice, ax.frame))
+        self.fig.canvas.mpl_connect('key_press_event', self.process_key)
+        self._update_suptitle()
+        self.fig.tight_layout()
+        plt.show()
 
-  def remove_keymap_conflicts(self, new_keys_set):
-    """
-    Removes keymap conflicts with defined keys for slices and frames.
+    def _update_suptitle(self):
+        # Assumes synchronized navigation across all subplots
+        ref_ax = self.ax[0]
+        self.fig.suptitle(f'Slice {ref_ax.curr_slice}, Frame {ref_ax.curr_frame}')
 
-    Args:
-      new_keys_set (set): Set of new keys to avoid conflicts with.
-    """
-    for prop in plt.rcParams:
-      if prop.startswith('keymap.'):
-        keys = plt.rcParams[prop]
-        remove_list = set(keys) & new_keys_set
-        for key in remove_list:
-          keys.remove(key)
+    def process_key(self, event):
+        if event.key not in [self.next_slice_key, self.previous_slice_key, 
+                             self.next_frame_key, self.previous_frame_key]:
+            return
 
-  def process_key(self, event):
-    """
-    Processes key press events for navigation.
+        for ax in self.ax[:len(self._images)]:
+            if event.key == self.next_slice_key:
+                ax.curr_slice = (ax.curr_slice + 1) % ax.im_data.shape[2]
+            elif event.key == self.previous_slice_key:
+                ax.curr_slice = (ax.curr_slice - 1) % ax.im_data.shape[2]
+            elif event.key == self.next_frame_key:
+                ax.curr_frame = (ax.curr_frame + 1) % ax.im_data.shape[3]
+            elif event.key == self.previous_frame_key:
+                ax.curr_frame = (ax.curr_frame - 1) % ax.im_data.shape[3]
+            
+            ax.get_images()[0].set_data(ax.im_data[..., ax.curr_slice, ax.curr_frame])
 
-    Args:
-      event (Event): The key press event.
-    """
-    fig = event.canvas.figure
-    for ax in fig.axes[:len(self._images)]:
-      if event.key == self.previous_slice_key:
-        self.previous_slice(ax)
-      elif event.key == self.next_slice_key:
-        self.next_slice(ax)
-      elif event.key == self.previous_frame_key:
-        self.previous_frame(ax)
-      elif event.key == self.next_frame_key:
-        self.next_frame(ax)
-    fig.canvas.draw()
+        self._update_suptitle()
+        self.fig.canvas.draw_idle()
+
+    def remove_keymap_conflicts(self, new_keys_set):
+        for prop in plt.rcParams:
+            if prop.startswith('keymap.'):
+                keys = plt.rcParams[prop]
+                for key in (set(keys) & new_keys_set):
+                    keys.remove(key)
